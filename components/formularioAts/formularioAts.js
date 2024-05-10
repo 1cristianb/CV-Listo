01f1958a4f9f4dd5add3bb6aa8b4495a8ba48038
1cristianb <crboschi@alumno.unlam.edu.ar>
2024-05-10 19:23:02 -0300

    se modificó espacios en el pdf

--- components/formularioAts/formularioAts.js
+++ components/formularioAts/formularioAts.js
@@ -2,16 +2,18 @@
 import { useState } from 'react';
 import jsPDF from 'jspdf';
 import ExperienciaAts from './experienciaAts';
+import EducacionAts from './educacionAts';
 
 const FormularioAts = () => {
     const [formData, setFormData] = useState({
         name: '',
         title: '',
+        profileDescription: '',
         email: '',
         phone: '',
         city: '',
         experiences: [],
-        education: '',
+        education: [],
         skills: '',
     });
 
@@ -27,53 +29,76 @@ const FormularioAts = () => {
 
     const generatePDF = (formData) => {
         const doc = new jsPDF();
+        const lineStart = 20; // Inicio de la línea
+        const lineEnd = doc.internal.pageSize.getWidth() - 20; // Final de la línea
+        const lineLength = lineEnd - lineStart; // Longitud de la línea
 
         // Agregar el título
+        let yPos = 20;
         doc.setFontSize(40);
         doc.setFont("georgia", "bold");
-        doc.text(formData.name, 20, 20);
-
+        doc.text(formData.name, lineStart, yPos);
+        yPos += 10;
         doc.setFont("georgia", "semi-bold");
         doc.setFontSize(18);
-        doc.text(formData.title.toUpperCase(), 20, 30);
+        doc.text(formData.title.toUpperCase(), lineStart, yPos);
+        yPos += 10;
 
-        doc.line(20, 38, 190, 38);
+        // Agregar la descripción de perfil
+        doc.setFontSize(10);
+        const lineasPerfil = doc.splitTextToSize(formData.profileDescription, 170);
+        doc.text(lineasPerfil, lineStart, yPos);
+        yPos += lineasPerfil.length * 5 + 2; // Ajustar la posición vertical después de la descripción de perfil
 
-        doc.setFontSize(12);
-        doc.text(formData.email, 20, 45);
-        doc.text(formData.phone, 90, 45);
-        doc.text(formData.city, 160, 45);
-
-        doc.line(20, 49, 190, 49);
+        doc.line(lineStart, yPos, lineEnd, yPos);
+        yPos += 5;
+        doc.text(formData.email, lineStart, yPos);
+        doc.text(formData.phone, (lineStart + lineLength / 2) - 15, yPos); // Teléfono centrado
+        doc.text(formData.city, lineEnd, yPos, null, null, 'right'); // Ciudad alineada a la derecha
+        yPos += 3; // Ajustar la posición vertical después del correo electrónico, teléfono y ciudad
+        doc.line(lineStart, yPos, lineEnd, yPos);
+        yPos += 11; // Ajustar la posición vertical para la sección de experiencia
 
         // Agregar la sección de experiencia
         doc.setFontSize(16);
-        doc.text('EXPERIENCIA', 20, 60);
+        doc.text('EXPERIENCIA', lineStart, yPos);
+        yPos += 8;
         doc.setFontSize(12);
-        let yPos = 68;
         formData.experiences.forEach((experience, index) => {
+            doc.setFontSize(12);
             doc.setFont("georgia", "bold");
-            doc.text(`${experience.position} - ${experience.company}`, 20, yPos);
+            doc.text(`${experience.position} - ${experience.company}`, lineStart, yPos);
             doc.setFont("georgia", "normal");
-            doc.text(`${experience.dateStart} - ${experience.dateFinish}`, 20, yPos + 8);
-            const lineas = doc.splitTextToSize(experience.description, 180);
-            doc.text(lineas, 20, yPos + 16);
-            yPos += 20 + lineas.length * 8;
+            doc.text(`${experience.dateStart} / ${experience.dateFinish}`, lineStart, yPos + 5);
+            doc.setFontSize(10);
+            const lineas = doc.splitTextToSize(experience.description, 170);
+            doc.text(lineas, lineStart, yPos + 10);
+            yPos += 8 + lineas.length * 8;
         });
 
         // Agregar la sección de educación
         doc.setFontSize(16);
-        doc.text('EDUCACIÓN', 20, yPos + 16);
+        doc.text('EDUCACIÓN', lineStart, yPos + 7);
+        yPos += 16;
         doc.setFontSize(12);
-        const lineasEducacion = doc.splitTextToSize(formData.education, 180);
-        doc.text(lineasEducacion, 20, yPos + 24);
+        formData.education.forEach((education, index) => {
+            doc.setFont("georgia", "normal");
+            doc.text(`${education.dateStart} / ${education.dateFinish}`, lineStart, yPos);
+            yPos += 5;
+            doc.setFont("georgia", "bold");
+            doc.text(`${education.title} - ${education.area} - ${education.university}`, lineStart, yPos);
+            yPos += 7;
+        });
 
         // Agregar la sección de aptitudes
+        doc.setFont("georgia", "normal");
         doc.setFontSize(16);
-        doc.text('APTITUDES', 20, yPos + 40 + lineasEducacion.length * 8);
-        doc.setFontSize(12);
-        const lineasAptitudes = doc.splitTextToSize(formData.skills, 180);
-        doc.text(lineasAptitudes, 20, yPos + 48 + lineasEducacion.length * 8);
+        doc.text('APTITUDES', lineStart, yPos + 10);
+        yPos += 18;
+        doc.setFontSize(10);
+        const lineasAptitudes = doc.splitTextToSize(formData.skills, 170);
+        doc.text(lineasAptitudes, lineStart, yPos);
+        yPos += lineasAptitudes.length * 8;
 
         // Guardar el PDF
         doc.save('curriculum.pdf');
@@ -105,6 +130,17 @@ const FormularioAts = () => {
                     onChange={handleChange}
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="profileDescription" className="block text-gray-700 font-bold mb-2">
+                    Descripción del perfil
+                </label>
+                <textarea
+                    name="profileDescription"
+                    value={formData.profileDescription}
+                    onChange={handleChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                ></textarea>
             </div>
 
             <div className="mb-4">
@@ -147,17 +183,7 @@ const FormularioAts = () => {
 
             <ExperienciaAts formData={formData} setFormData={setFormData} />
 
-            <div className="mb-4">
-                <label htmlFor="education" className="block text-gray-700 font-bold mb-2">
-                    Educación
-                </label>
-                <textarea
-                    name="education"
-                    value={formData.education}
-                    onChange={handleChange}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                ></textarea>
-            </div>
+            <EducacionAts formData={formData} setFormData={setFormData} />
 
             <div className="mb-4">
                 <label htmlFor="skills" className="block text-gray-700 font-bold mb-2">
